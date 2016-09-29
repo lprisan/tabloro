91fd984da3ab6d62eaf64ee4e7c809578cbca160
lprisan <lprisan@hotmail.com>
2016-09-29 20:41:20 +0200

    Implemented most of the card generation from GSpreadsheet

--- scripts/card-generation/generateCards.py
+++ scripts/card-generation/generateCards.py
@@ -1,89 +1,281 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Mon Sep 26 20:41:56 2016
 
-Requires packages gspread, oauth2client, pyopenssl
+Requires packages gspread, oauth2client, pyopenssl, lxml
 
 @author: lprisan.itd@gmail.com (the credentials are generated with this user)
 """
-
 import os
-
-os.chdir("/media/sf_shared/4ts")
-
-# If using oauth2client <2.0+
-#import json
-#import gspread
-#from oauth2client.client import SignedJwtAssertionCredentials
-#json_key = json.load(open('Remote-GSpreadsheet-Access-25f314b90a7f.json'))
-#scope = ['https://spreadsheets.google.com/feeds']
-#credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
-#gsc = gspread.authorize(credentials)
-
-
-# If using oauth2client 2.0+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-scope = ['https://spreadsheets.google.com/feeds']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('Remote-GSpreadsheet-Access-25f314b90a7f.json', scope)
-gsc = gspread.authorize(credentials)
-
-wks = gsc.open_by_url("https://docs.google.com/spreadsheets/d/17aQHUptQw1W779G23sNhume_IS5h2TMmsOJVh2vo4B0/edit#gid=0").sheet1
-
-#cells = wks.range('A1:F3')
-cardinstances = []
-
-# For now, get only a few cells
-for i in range(2,4):
-    rowvals = wks.row_values(i)
-    print(rowvals)
-    cardtype = rowvals[0]
-    cardtitle = rowvals[1]
-    cardfront = rowvals[2]
-    cardback_L = rowvals[3]
-    cardback_R = rowvals[4]
-    cardtags = filter(None, rowvals[5:25]) # May be multiple instances, we should check a long array and keep only the nonempty rows
-    for j in range(0,len(cardtags)): # For each tag, we create a quasi-identical card instance
-        card = {}
-        card['type'] = cardtype
-        card['title'] = cardtitle
-        card['front'] = cardfront
-        card['back_L'] = cardback_L
-        card['back_R'] = cardback_R
-        card['tag'] = int(cardtags[j])
-        cardinstances.append(card)
-    
-# TODO: Manipulate the SVG/XML with the cells' contents, and save a copy
+import sys
 from lxml import etree
+from subprocess import call
 
-iconfiles = [{'Technique': './technique.png'}]
-bgcolors = {'Technique': '#adcdea'}
-TEMPLATE_BGCOLOR = '#adcdea'
+#print 'Number of arguments:', len(sys.argv), 'arguments.'
+#print 'Argument List:', str(sys.argv)
 
-# Generate the digital card
-card = cardinstances[0]
-tree = etree.parse('digital_card_native.svg')
-# Modify bg color
-bgrect = tree.xpath('.//*[local-name()="rect"]')[0]
-bgrect.attrib['style'] = bgrect.attrib['style'].replace(TEMPLATE_BGCOLOR,bgcolors[card['type']])
-#nsmap = tree.getroot().nsmap.copy()
-#nsmap['xmlns'] = nsmap.pop(None)
-# bgrect = tree.xpath('.//xmlns:rect', namespaces=nsmap)[0]
-# Modify icon
+# Some constants for the program to work
+SPREADSHEET_URL_EN = "https://docs.google.com/spreadsheets/d/17aQHUptQw1W779G23sNhume_IS5h2TMmsOJVh2vo4B0/edit#gid=0"
+SPREADSHEET_URL_IT = ""
+TEMPLATE_DIGITAL = "./templates/digital_card_template.svg"
+TEMPLATE_PRINT_FRONT = "./templates/printable_card_template_front.svg"
+TEMPLATE_PRINT_BACK_PLAIN = "./templates/printable_card_template_back_plain.svg"
+TEMPLATE_PRINT_BACK_TABLE = "./templates/printable_card_template_back_table.svg"
+iconfiles = {'Technique': '../../icons/technique.png',
+             'Task': '../../icons/task.png',
+             'Team': '../../icons/team.png',
+             'Technology': '../../icons/technology.png',
+             'Wildcard': '../../icons/joker.png'}
+bgcolors = {'Technique': '#adcdea',
+            'Wildcard': '#ffffff',
+            'Task': '#f57d86', # Originally '#f14c58', but looked too dark
+            'Team': '#fee964',
+            'Technology': '#9edb96'}
+TEMPLATE_BGCOLOR = '#adcdea' #Needed to substitute the background colors in the svg
 
-# Modify type
 
-# Modify title
 
-# write the modified file to a SVG file
-tree.write()
+def extractFromSpreadsheet(url):
+    """Connects to a (authorized) google spreadsheet, and extracts the data 
+    from it about the card titles, descriptions, types etc.
+
+    Returns list of dictionaries, each one with the card's data."""
+    # If using oauth2client 2.0+
+    print('Trying to connect to '+url)
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('Remote-GSpreadsheet-Access-25f314b90a7f.json', scope)
+    gsc = gspread.authorize(credentials)
+    wks = gsc.open_by_url(SPREADSHEET_URL_EN).sheet1
+    #cells = wks.range('A1:F3')
+    cardinstances = [] # Here we'll store the cards from the spreadsheet
+    i = 2
+    while True:
+        print('Extracting row %d' % i)        
+        rowvals = wks.row_values(i)
+        #print(rowvals)
+        cardtype = rowvals[0]
+        if(len(cardtype)==0): #When a card has no type, we have finished
+            break
+        else:
+            cardtitle = rowvals[1]
+            carddesc = rowvals[2]
+            cardind_plain = rowvals[3]
+            cardind_task1 = rowvals[4]
+            cardind_team1 = rowvals[5]
+            cardind_tech1 = rowvals[6]
+            cardind_time1 = rowvals[7]
+            cardind_task2 = rowvals[8]
+            cardind_team2 = rowvals[9]
+            cardind_tech2 = rowvals[10]
+            cardind_time2 = rowvals[11]
+            cardind_task3 = rowvals[12]
+            cardind_team3 = rowvals[13]
+            cardind_tech3 = rowvals[14]
+            cardind_time3 = rowvals[15]
+            cardtags = filter(None, rowvals[16:35]) # May be multiple instances, we should check a long array and keep only the nonempty rows
+            for j in range(0,len(cardtags)): # For each tag, we create a quasi-identical card instance
+                card = {}
+                card['type'] = cardtype
+                card['title'] = cardtitle
+                card['description'] = carddesc
+                card['ind_plain'] = cardind_plain
+                card['ind_task1'] = cardind_task1
+                card['ind_team1'] = cardind_team1
+                card['ind_tech1'] = cardind_tech1
+                card['ind_time1'] = cardind_time1
+                card['ind_task2'] = cardind_task2
+                card['ind_team2'] = cardind_team2
+                card['ind_tech2'] = cardind_tech2
+                card['ind_time2'] = cardind_time2
+                card['ind_task3'] = cardind_task3
+                card['ind_team3'] = cardind_team3
+                card['ind_tech3'] = cardind_tech3
+                card['ind_time3'] = cardind_time3
+                card['tag'] = int(cardtags[j])
+                cardinstances.append(card)
+                #print('Adding new card, now %d' % len(cardinstances))                
+            i = i+1
+    return cardinstances
+
+
+
+def loadModifyDigitalSVG(svgfile, card):
+    """Loads a digital card svg file, and loads it with the data from a card
+    dictionary.
+
+    Returns an element tree (lxml) of the modified svg file."""    # Generate the digital card
+    tree = etree.parse(svgfile)
+    # Modify bg color
+    bgrect = tree.xpath('.//*[local-name()="rect"]')[0]
+    bgrect.attrib['style'] = bgrect.attrib['style'].replace(TEMPLATE_BGCOLOR,bgcolors[card['type']])
+    #nsmap = tree.getroot().nsmap.copy()
+    #nsmap['xmlns'] = nsmap.pop(None)
+    # bgrect = tree.xpath('.//xmlns:rect', namespaces=nsmap)[0]
+    # Modify icon
+    icon = tree.xpath('.//*[local-name()="image"]')[0]
+    icon.attrib['{http://www.w3.org/1999/xlink}href'] = iconfiles[card['type']]
+    # Modify type
+    typetext = tree.xpath('.//*[@id="tspan3879"]')[0]
+    typetext.text = card['type']
+    # Modify title
+    titletext = tree.xpath('.//*[@id="flowPara3895"]')[0]
+    titletext.text = card['title']
+    return tree
+
+
+def loadModifyFrontSVG(svgfile, card):
+    """Loads a printable front card svg file, and loads it with the data from a card
+    dictionary.
+
+    Returns an element tree (lxml) of the modified svg file."""    # Generate the digital card
+    tree = etree.parse(svgfile)
+    # Modify bg color
+    bgrect = tree.xpath('.//*[local-name()="rect"]')[0]
+    bgrect.attrib['style'] = bgrect.attrib['style'].replace(TEMPLATE_BGCOLOR,bgcolors[card['type']])
+    #nsmap = tree.getroot().nsmap.copy()
+    #nsmap['xmlns'] = nsmap.pop(None)
+    # bgrect = tree.xpath('.//xmlns:rect', namespaces=nsmap)[0]
+    # Modify icons
+    icon = tree.xpath('.//*[@id="image3933"]')[0]
+    icon.attrib['{http://www.w3.org/1999/xlink}href'] = iconfiles[card['type']]
+    icon2 = tree.xpath('.//*[@id="image3933-6"]')[0]
+    icon2.attrib['{http://www.w3.org/1999/xlink}href'] = iconfiles[card['type']]
+    # Modify tag
+    tag = tree.xpath('.//*[@id="image4113"]')[0]
+    tag.attrib['{http://www.w3.org/1999/xlink}href'] = '../../tags/big-noborder/'+str(card['tag'])+'.png'
+    # Modify type
+    typetext = tree.xpath('.//*[@id="tspan3946"]')[0]
+    typetext.text = card['type']
+    typetext2 = tree.xpath('.//*[@id="tspan3946-1"]')[0]
+    typetext2.text = card['type']
+    # Modify titleS
+    titletext = tree.xpath('.//*[@id="tspan4016"]')[0]
+    titletext.text = card['title']
+    titletext2 = tree.xpath('.//*[@id="tspan4016-0"]')[0]
+    titletext2.text = card['title']
+    # Modify text paragraph
+    fronttext = tree.xpath('.//*[@id="flowPara4200"]')[0]
+    # If there is a \n\n, we only put the text before it    
+    fronttext.text = card['description'].split("\n\n")[0]
+    return tree
+
+
+# MAIN PROGRAM
+if __name__ == '__main__':
+    # If no args, or the first arg is --en, we do the cards from the english spreadsheet 
+    cardinstances = [] # Here we'll store the cards from the spreadsheet
+    if(len(sys.argv)==1 or sys.argv[1]=='--en'):
+        cardinstances = extractFromSpreadsheet(SPREADSHEET_URL_EN)
+    elif(sys.argv[1]=="--it"):
+        cardinstances = extractFromSpreadsheet(SPREADSHEET_URL_IT)
+    print('Read %d cards from spreadsheet' % len(cardinstances))
+    dir_path = os.path.dirname(os.path.realpath(__file__)) # This script's path
+    os.chdir(dir_path)
+    print('Working in '+dir_path)
+    #print(cardinstances)
+    # For each card, we generate everything
+    for card in cardinstances:
+        digitalsvg = loadModifyDigitalSVG(TEMPLATE_DIGITAL, card)
+        # write the modified file to a SVG file
+        filenameD = './output/digital/'+card['title']+'_'+str(card['tag'])+'_DIGITAL.svg'
+        print('Saving digital card at '+filenameD)
+        digitalsvg.write(filenameD)
+        # Export the file to png for uploading in tabloro
+        call(["inkscape", filenameD, "--export-png="+filenameD+".png", "-h140"]) # Should be 100x140 max
+        frontsvg = loadModifyFrontSVG(TEMPLATE_PRINT_FRONT, card)
+        # write the modified file to a SVG file
+        filenameF = './output/printable/'+card['title']+'_'+str(card['tag'])+'_FRONT.svg'
+        print('Saving front card at '+filenameF)
+        frontsvg.write(filenameF)
+        # Export the file to pdf for pasting and printing
+        call(["inkscape", filenameF, "--export-pdf="+filenameF+".pdf"])
+        #TODO: Do the back card, depending on the type
+        #TODO: Paste both pdfs into a single pdf
+        #call(["pdftk", "frontcard.pdf", "frontcard.pdf", "cat", "output", "doublecard.pdf"])
+
+
+    
+
+# For each card, we generate everything
+#for card in cardinstances:
+#    # Generate the digital card
+#    tree = etree.parse('digital_card_template.svg')
+#    # Modify bg color
+#    bgrect = tree.xpath('.//*[local-name()="rect"]')[0]
+#    bgrect.attrib['style'] = bgrect.attrib['style'].replace(TEMPLATE_BGCOLOR,bgcolors[card['type']])
+#    #nsmap = tree.getroot().nsmap.copy()
+#    #nsmap['xmlns'] = nsmap.pop(None)
+#    # bgrect = tree.xpath('.//xmlns:rect', namespaces=nsmap)[0]
+#    # Modify icon
+#    icon = tree.xpath('.//*[local-name()="image"]')[0]
+#    icon.attrib['{http://www.w3.org/1999/xlink}href'] = iconfiles[card['type']]
+#    # Modify type
+#    typetext = tree.xpath('.//*[@id="tspan3879"]')[0]
+#    typetext.text = card['type']
+#    # Modify title
+#    titletext = tree.xpath('.//*[@id="flowPara3895"]')[0]
+#    titletext.text = card['title']
+#    # write the modified file to a SVG file
+#    filenameD = './'+card['title']+'_'+str(card['tag'])+'_DIGITAL.svg'
+#    print('Saving digital card at '+filenameD)
+#    tree.write(filenameD)
+#    # Do the same for the FRONT printable cards
+#    tree = etree.parse('printable_card_template_front.svg')
+#    # Modify bg color
+#    bgrect = tree.xpath('.//*[@id="rect3862"]')[0]
+#    bgrect.attrib['style'] = bgrect.attrib['style'].replace(TEMPLATE_BGCOLOR,bgcolors[card['type']])
+#    #nsmap = tree.getroot().nsmap.copy()
+#    #nsmap['xmlns'] = nsmap.pop(None)
+#    # bgrect = tree.xpath('.//xmlns:rect', namespaces=nsmap)[0]
+#    # Modify icon
+#    icon = tree.xpath('.//*[@id="image3933"]')[0]
+#    icon.attrib['{http://www.w3.org/1999/xlink}href'] = iconfiles[card['type']]
+#    # Modify tag
+#    tag = tree.xpath('.//*[@id="image4113"]')[0]
+#    tag.attrib['{http://www.w3.org/1999/xlink}href'] = './'+str(card['tag'])+'.png'
+#    # Modify type
+#    typetext = tree.xpath('.//*[@id="tspan3946"]')[0]
+#    typetext.text = card['type']
+#    # Modify titleS
+#    titletext = tree.xpath('.//*[@id="tspan4016"]')[0]
+#    titletext.text = card['title']
+#    titletext2 = tree.xpath('.//*[@id="tspan4016-0"]')[0]
+#    titletext2.text = card['title']
+#    # Modify text paragraph
+#    fronttext = tree.xpath('.//*[@id="flowPara4200"]')[0]
+#    fronttext.text = card['front']
+#    # write the modified file to a SVG file
+#    filenameP = './'+card['title']+'_'+str(card['tag'])+'_FRONT.svg'
+#    print('Saving front card at '+filenameP)
+#    tree.write(filenameP)
+#    # Do the same for the BACK printable cards
+#    tree = etree.parse('printable_card_template_back.svg')
+#    # Modify bg color
+#    bgrect = tree.xpath('.//*[@id="rect3862"]')[0]
+#    bgrect.attrib['style'] = bgrect.attrib['style'].replace(TEMPLATE_BGCOLOR,bgcolors[card['type']])
+#    #nsmap = tree.getroot().nsmap.copy()
+#    #nsmap['xmlns'] = nsmap.pop(None)
+#    # bgrect = tree.xpath('.//xmlns:rect', namespaces=nsmap)[0]
+#    # Modify text paragraph
+#    # TODO: Each paragraph is translated into a flowPara element!!
+##    backLtext = tree.xpath('.//*[@id="flowPara3075"]')[0]
+##    backLtext.text = card['back_L']
+##    backRtext = tree.xpath('.//*[@id="flowPara3075"]')[0]
+##    backRtext.text = card['back_R']
+##    # write the modified file to a SVG file
+##    filename = './'+card['title']+'_'+str(card['tag'])+'_FRONT.svg'
+##    print('Saving front card at '+filename)
+##    tree.write(filename)
+#    call(["inkscape", "/media/sf_shared/4ts/"+filenameD, "--export-png="+filenameD+".png"])
+#    call(["inkscape", "/media/sf_shared/4ts/"+filenameP, "--export-pdf="+filenameP+".pdf"])
+#    print("Finished writing files "+filenameD+" and "+filenameP)
+
+
 
 # Convert the SVGs to PDF or PNG, and join them for printing
-from subprocess import call
-call(["inkscape", "/media/sf_shared/4ts/carte_azzurre_Technique_new_oriz_native.svg", "--export-pdf=frontcard.pdf"])
-call(["pdftk", "frontcard.pdf", "frontcard.pdf", "cat", "output", "doublecard.pdf"])
-
-# Do the same for the digital version of the cards
-
+#call(["pdftk", "frontcard.pdf", "frontcard.pdf", "cat", "output", "doublecard.pdf"])
 
 # Upload the images to the platform via S3?
