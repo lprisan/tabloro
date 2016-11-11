df72ef5340a3e3e17c07ef533fd016717c6eddb3
lprisan <lprisan@hotmail.com>
2016-11-11 19:15:12 +0200

    Added a few more useful mongo commands

--- scripts/database.sh
+++ scripts/database.sh
@@ -7,5 +7,10 @@ mongodump --db noobjs_dev
 mv dump dump-2016-06-22
 tar -zcvf 20160622-tabloro-noobjs_dev.tar.gz dump-2016-06-22/
 
-# Restore the database from a dump
+# Restore the database from a dump. Normally there can be conflicts if restoring over existing one,
+# so we can remove the previous state MAKE SURE YOU ARE NOT DELETING ANYTHING IMPORTANT!!!
+mongo
+use noobjs_dev
+db.dropDatabase()
+quit()
 mongorestore dump-2016-06-22/
